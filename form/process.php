8620fc31f6eccaa442368a2591828dac0d3346c4
vatin-alexis <alexis.vatin1@gmail.com>
2021-05-05 15:41:47 +0200

    formulaire php pdo

--- form/process.php
+++ form/process.php
@@ -7,8 +7,8 @@ if(isset($_POST['insert'])){
   try {
   // se connecter à mysql
   $pdo = new PDO("mysql:host=$host;dbname=$dbname","$username","$password");
-  } catch (PDOException $exc) {
-    echo $exc->getMessage();
+  } catch (PDOException $e) {
+    echo $e->getMessage();
     exit();
   }
   // récupérer les valeurs 
